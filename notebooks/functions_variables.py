eafccf4682da86234b22f5e85c9c4d15c49b389c
mahikajain20 <mahika.jain@mail.utoronto.ca>
2024-06-01 12:50:42 -0400

    final changes 1

--- notebooks/functions_variables.py
+++ notebooks/functions_variables.py
@@ -70,4 +70,125 @@ def extract_info(result):
 
     return info
 
+def custom_cross_validation_no_city(training_data, n_splits =5):
+    assert isinstance(training_data, pd.DataFrame), "training_data should be a DataFrame"
+    assert isinstance(n_splits, int), "n_splits should be an integer"
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+    training_folds = []
+    validation_folds = []
+    for train_index, val_index in kf.split(training_data):
+        training_folds.append(training_data.iloc[train_index])
+        validation_folds.append(training_data.iloc[val_index])
 
+    return training_folds, validation_folds
+
+# develop your custom functions here
+
+def custom_cross_validation(training_data, n_splits =5):
+    '''creates n_splits sets of training and validation folds
+
+    Args:
+      training_data: the dataframe of features and target to be divided into folds
+      n_splits: the number of sets of folds to be created
+
+    Returns:
+      A tuple of lists, where the first index is a list of the training folds, 
+      and the second the corresponding validation fold
+
+    Example:
+        >>> output = custom_cross_validation(train_df, n_splits = 10)
+        >>> output[0][0] # The first training fold
+        >>> output[1][0] # The first validation fold
+        >>> output[0][1] # The second training fold
+        >>> output[1][1] # The second validation fold... etc.
+    '''
+    assert isinstance(training_data, pd.DataFrame), "training_data should be a DataFrame"
+    assert isinstance(n_splits, int), "n_splits should be an integer"
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+    training_folds = []
+    validation_folds = []
+    for train_index, val_index in kf.split(training_data):
+        
+        #Compute city means
+        city_means = training_folds[-1].groupby('city')['sold_price'].mean().rename('mean_price_city').reset_index()
+
+        #Merge city means with training and validation folds
+        training_folds[-1] = training_folds[-1].merge(city_means, on='city', how='left')
+        validation_folds[-1] = validation_folds[-1].merge(city_means, on='city', how='left')
+        
+        training_folds.append(training_data.iloc[train_index])
+        validation_folds.append(training_data.iloc[val_index])
+
+    return training_folds, validation_folds
+
+def hyperparameter_search(training_folds, validation_folds, param_grid, name):
+    '''outputs the best combination of hyperparameter settings in the param grid, 
+    given the training and validation folds
+
+    Args:
+      training_folds: the list of training fold dataframes
+      validation_folds: the list of validation fold dataframes
+      param_grid: the dictionary of possible hyperparameter values for the chosen model
+
+    Returns:
+      A list of the best hyperparameter settings based on the chosen metric
+
+    Example:
+        >>> param_grid = {
+          'max_depth': [None, 10, 20, 30],
+          'min_samples_split': [2, 5, 10],
+          'min_samples_leaf': [1, 2, 4],
+          'max_features': ['sqrt', 'log2']} # for random forest
+        >>> hyperparameter_search(output[0], output[1], param_grid = param_grid) 
+        # assuming 'ouput' is the output of custom_cross_validation()
+        [20, 5, 2, 'log2'] # hyperparams in order
+    '''
+    assert isinstance(training_folds, list), "training_folds should be a list"
+    assert isinstance(validation_folds, list), "validation_folds should be a list"
+    assert isinstance(param_grid, dict), "param_grid should be a dictionary"
+    best_hyperparameters = []
+    best_score = float('inf')
+    for params in ParameterGrid(param_grid):
+        #print(params)
+        scores = []
+        for t_fold, v_fold in zip(training_folds, validation_folds):
+            if name == 'DecisionTree':
+                model = DecisionTreeRegressor(**params, random_state=42)
+            if name == 'RandomForest':
+                model = RandomForestRegressor(**params, random_state=42)
+            if name == 'XGBoost':
+                #dtrain = xgb.DMatrix(data=t_fold, label=v_fold, enable_categorical=True)
+                model = XGBRegressor(**params, random_state=42)
+  
+                
+            x_train = t_fold.drop(columns='sold_price')
+            y_train = t_fold['sold_price']
+            x_val = v_fold.drop(columns='sold_price')
+            y_val = v_fold['sold_price']
+
+            model.fit(x_train, y_train)
+            preds = model.predict(x_val)
+            score = np.sqrt(mean_squared_error(y_val, preds))
+            scores.append(score)
+        if np.mean(scores) < best_score:
+            best_score = np.mean(scores)
+            best_hyperparameters = params
+        
+    return best_hyperparameters, best_score
+
+# # Create unit tests
+# def test_functions(df):
+
+#     # Test custom_cross_validation
+#     training_folds, validation_folds = custom_cross_validation(df, n_splits=2)
+#     assert isinstance(training_folds, list), "Output of custom_cross_validation should be a list"
+#     assert isinstance(validation_folds, list), "Output of custom_cross_validation should be a list"
+
+#     # Test hyperparameter_search
+#     param_grid = {
+#         'max_depth': [None, 10],
+#         'min_samples_split': [2, 5],
+#     }
+#     best_hyperparameters, best_score = hyperparameter_search(training_folds, validation_folds, param_grid, 'DecisionTree')
+#     assert isinstance(best_hyperparameters, dict), "Output of hyperparameter_search should be a dictionary"
+#     assert isinstance(best_score, float), "Output of hyperparameter_search should be a float"
